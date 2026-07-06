af37ee78eb00a07f3360534f115a57946bb111d3
choidev777-bit <choidev777@gmail.com>
2026-07-06 11:13:05 +0900

    Wrap interactive input in add.py under __main__ check

--- add.py
+++ add.py
@@ -1,8 +1,9 @@
 def add(a, b):
     return a + b
 
-input1 = int(input("Enter first number: "))
-input2 = int(input("Enter second number: "))
+if __name__ == '__main__':
+    input1 = int(input("Enter first number: "))
+    input2 = int(input("Enter second number: "))
 
-res1 = add(input1, input2)
-print(res1)
+    res1 = add(input1, input2)
+    print(res1)

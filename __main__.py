f1e470dd3e01502997d325851034aa9b537b1a08
choidev777-bit <choidev777@gmail.com>
2026-07-06 11:13:36 +0900

    Create calculator package entrypoints __init__.py and __main__.py

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from add import add
+from minus import minus
+from multiplication import multiplication
+from division import division
+
+if __name__ == '__main__':
+    print("--- 계산기 패키지 실행 ---")
+    print(f"3 + 5 = {add(3, 5)}")
+    print(f"10 - 4 = {minus(10, 4)}")
+    print(f"3 * 5 = {multiplication(3, 5)}")
+    print(f"10 / 2 = {division(10, 2)}")
+    print("10 / 0 = ", end="")
+    division(10, 0)

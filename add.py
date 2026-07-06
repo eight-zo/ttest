554097dc3f8eb45880fc8f5d0f9287e37f69d6c8
Yerin <kyr0813kim@gmail.com>
2026-07-06 11:18:28 +0900

    merge add.py

--- add.py
+++ add.py
@@ -1,8 +1,2 @@
 def add(a, b):
     return a + b
-
-""" input1 = int(input("Enter first number: "))
-input2 = int(input("Enter second number: ")) """
-
-""" res1 = add(3, 5) """
-""" print(res1) """

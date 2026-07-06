551e3f153f77e7164a2a96b5be8eb4422abcd2ea
YerinKim13 <kyr0813kim@gmail.com>
2026-07-06 11:26:43 +0900

    Remove main execution block from add.py
    Remove user input and print statements from add.py

--- add.py
+++ add.py
@@ -1,9 +1,3 @@
 def add(a, b):
     return a + b
 
-if __name__ == '__main__':
-    input1 = int(input("Enter first number: "))
-    input2 = int(input("Enter second number: "))
-
-    res1 = add(input1, input2)
-    print(res1)
